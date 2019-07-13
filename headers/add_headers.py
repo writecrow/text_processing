db6a962e63e740b246f8c530a3257281265aeb24
Adriana Picoral <adriana@on-campus-10-138-78-206.vpn.arizona.edu>
2019-07-13 14:34:31 -0700

    fixed bug with backslash file format

--- headers/add_headers.py
+++ headers/add_headers.py
@@ -55,7 +55,8 @@ def add_header_to_file(filename, master, overwrite=False):
         else:
             print('Adding headers to file ' + textfile)
             textfile = open(filename, 'r')
-            filename_parts2 = filename.split('/')
+            not_windows_filename = re.sub(r'\\', r'/', filename)
+            filename_parts2 = not_windows_filename.split('/')
 
             course = filtered_master2['Catalog Nbr'].to_string(index=False)
             assignment = filename_parts2[7][:2]
