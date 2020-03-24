7789d32bd48e0769b26eee4a2ff2fa5bbf4af1b1
alekseynovikov78 <alekseynovikov@email.arizona.edu>
2020-03-24 13:46:19 -0700

    updated NAU script

--- headers/NAUface_headers.py
+++ headers/NAUface_headers.py
@@ -5,21 +5,6 @@ import re
 import os
 import pandas
 
-"""<Student ID: #####>
-<Country: CHN>
-<Language: >
-<Institution: Northern Arizona University>
-<Course: ENGL 105>
-<Mode: Face to Face>
-<Length: 16 weeks>
-<Assignment: AA>
-<Draft: F>
-<Course Year: 2016>
-<Course Semester: Fall>
-<Instructor: 3###>
-<End Header>"""
-
-
 
 # Define the way we retrieve arguments sent to the script.
 parser = argparse.ArgumentParser(description='Add Headers to Individual Textfile')
@@ -56,13 +41,14 @@ def process_file(filename):
         
         # if it is an english file (With length of 3)
         if len(filename_parts) > 2:
-            assignment = filename_part2.split("_")[1]
+            assignment_code = filename_part2.split("_")[1]
             
         else:
-            assignment = filename_part2.split("_")[1]
+            assignment_code = filename_part2.split("_")[1]
             shortened_filename = filename_part2.split("_")[0]
+            print(shortened_filename)
 
-            #assignment = re.sub(r"LA", r"Long Argument", assignment)
+            assignment = re.sub(r"LA", r"Long Argument", assignment_code)
             print("Assignment: ", assignment)
 
             instructor = filename_part2[:2]
@@ -82,89 +68,88 @@ def process_file(filename):
             language = re.sub(r"A", r"Arabic", language)
             print("Language: ", language)
 
-            student_ID = shortened_filename[-1]
-            
-            # add student_ID to dictionary of ids if not there already
-            student_ids_dictionary = add_id_to_dictionary(student_ids_dictionary, student_ID)
- 
-            crow_id = student_ids[student_ID]
-
+            student_ID = shortened_filename[-1]+instructor+semester0
             if shortened_filename[-2].isdigit():
-                student_ID = shortened_filename[-2] + shortened_filename[-1]
+                student_ID = shortened_filename[-2] + shortened_filename[-1]+instructor+semester0
             else:
                 pass
-                #student_ID = shortened_filename[-1]
-            print("Student ID: ", student_ID, crow_id)
-
+            print("Student_id:",student_ID)
             
+            # add student_ID to dictionary of ids if not there already
+            add_id_to_dictionary(student_ids_dictionary, student_ID)
+            print(student_ids_dictionary)
+ 
+            crow_id = student_ids_dictionary[student_ID]
 
-        textfile = open(filename, 'r')
+            print("Student ID: ",crow_id)
+    
 
-        output_filename = ''
-        output_filename += course_name
-        output_filename += '_'
-        output_filename += assignment
-        output_filename += '_'
-        output_filename += "DF"
-        output_filename += '_'
-        output_filename += "NA"
-        output_filename += '_'
-        output_filename += "NA"
-        output_filename += '_'
-        output_filename += "NA"
-        output_filename += '_'
-        output_filename += instructor
-        output_filename += crow_id
-        output_filename += semester0
-        output_filename += '_'
-        output_filename += "NAU"
-        output_filename += '.txt'
-        #output_filename = re.sub(r'\s', r'', output_filename)
-        #output_filename = re.sub(r'__', r'_NA_', output_filename)
+            textfile = open(filename, 'r')
 
-        old_folder = filename_clean[1]
-        new_folder = "files_with_headers"
-        path = os.path.join(cwd, new_folder, course_name, 'English', )
+            output_filename = ''
+            output_filename += course_name
+            output_filename += '_'
+            output_filename += assignment_code
+            output_filename += '_'
+            output_filename += "DF"
+            output_filename += '_'
+            output_filename += "NA"
+            output_filename += '_'
+            output_filename += "NA"
+            output_filename += '_'
+            output_filename += "NA"
+            output_filename += '_'
+            output_filename += str(crow_id)
+            output_filename += '_'
+            output_filename += "NAU"
+            output_filename += '.txt'
+            #output_filename = re.sub(r'\s', r'', output_filename)
+            #output_filename = re.sub(r'__', r'_NA_', output_filename)
 
-        if not os.path.exists(path):
-            os.makedirs(path)
+            old_folder = filename_clean[1]
+            new_folder = "files_with_headers"
+            cwd = os.getcwd()
+            path = os.path.join(cwd, new_folder, course_name, 'English/')
 
-        output_file = open(path + output_filename, 'w')
+            if not os.path.exists(path):
+                os.makedirs(path)
 
-        print("<Student ID: " + student_ID + ">", file = output_file)
-        print("<Country: " + "NA" + ">", file = output_file)
-        print("<Institution: " + "NAU" + ">", file = output_file)
-        print("<Course: ENGL " + "NA" + ">", file = output_file)
-        print("<Mode: " + "Face to Face" + ">", file = output_file)
-        print("<Length: " + "16 weeks" + ">", file = output_file)
-        print("<Assignment: " + assignment + ">", file = output_file)
-        print("<Draft: " + "DF" + ">", file = output_file)
-        print("<Year in School: " + "NA" + ">", file = output_file)
-        print("<Gender: " + "NA" + ">", file = output_file)
-        print("<Course Year: " + year + ">", file = output_file)
-        print("<Course Semester: " + semester + ">" , file = output_file)
-        print("<College: " + "NA" + ">", file = output_file)
-        print("<Program: " + "NA" + ">", file = output_file)
-        print("<Proficiency Exam: " + "NA" +">", file = output_file)
-        print("<Exam total: " + "NA" + ">", file = output_file)
-        print("<Exam reading: " + "NA" + ">", file = output_file)
-        print("<Exam listening: " + "NA" + ">", file = output_file)
-        print("<Exam speaking: " + "NA" + ">", file = output_file)
-        print("<Exam writing: " + "NA" + ">", file = output_file)
-        print("<Instructor: " + instructor + ">", file = output_file)
-        print("<Section: " + "NA" + ">", file = output_file)
-        print("<End Header>", file = output_file)
-        print("", file = output_file)
+            output_file = open(path + output_filename, 'w')
 
-        for line in textfile:
-            this_line = re.sub(r'\r?\n', r'\r\n', line)
-            if this_line != '\r\n':
-                new_line = re.sub(r'\s+', r' ', this_line)
-                new_line = new_line.strip()
-                print(new_line, file = output_file)
+            print("<Student ID: " + str(crow_id) + ">", file = output_file)
+            print("<Country: " + "NA" + ">", file = output_file)
+            print("<Institution: " + "NAU" + ">", file = output_file)
+            print("<Course: ENGL " + "NA" + ">", file = output_file)
+            print("<Mode: " + "Face to Face" + ">", file = output_file)
+            print("<Length: " + "16 weeks" + ">", file = output_file)
+            print("<Assignment: " + assignment + ">", file = output_file)
+            print("<Draft: " + "DF" + ">", file = output_file)
+            print("<Year in School: " + "NA" + ">", file = output_file)
+            print("<Gender: " + "NA" + ">", file = output_file)
+            print("<Course Year: " + year + ">", file = output_file)
+            print("<Course Semester: " + semester + ">" , file = output_file)
+            print("<College: " + "NA" + ">", file = output_file)
+            print("<Program: " + "NA" + ">", file = output_file)
+            print("<Proficiency Exam: " + "NA" +">", file = output_file)
+            print("<Exam total: " + "NA" + ">", file = output_file)
+            print("<Exam reading: " + "NA" + ">", file = output_file)
+            print("<Exam listening: " + "NA" + ">", file = output_file)
+            print("<Exam speaking: " + "NA" + ">", file = output_file)
+            print("<Exam writing: " + "NA" + ">", file = output_file)
+            print("<Instructor: " + instructor + ">", file = output_file)
+            print("<Section: " + "NA" + ">", file = output_file)
+            print("<End Header>", file = output_file)
+            print("", file = output_file)
 
-        output_file.close()
-        textfile.close()
+            for line in textfile:
+                this_line = re.sub(r'\r?\n', r'\r\n', line)
+                if this_line != '\r\n':
+                    new_line = re.sub(r'\s+', r' ', this_line)
+                    new_line = new_line.strip()
+                    print(new_line, file = output_file)
+
+            output_file.close()
+            textfile.close()
 
 
 def process_directory(directory_name):
