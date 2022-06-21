fb7e2d413f88837bdb818427acc2d82e3d5b2cfc
Mark Fullmer <mfullmer@gmail.com>
2022-06-21 13:04:35 -0700

    Rework header structure for v2 (group parsing still to do)

--- headers/add_headers_full_name.py
+++ headers/add_headers_full_name.py
@@ -129,6 +129,9 @@ def analyze_results(results):
         for i in duplicate_filenames:
             print('* ' + i)
 
+def add_heading(key, value, filename):
+    print("<" + key + ": " + value + ">", file=filename)
+
 
 def add_header_to_file(filepath, metadata, results):
     # print('Adding headers to file ' + filepath)
@@ -235,33 +238,41 @@ def add_header_to_file(filepath, metadata, results):
 
     hash = create_file_hash(filepath)
     results.append([filepath, hash, output_filename])
-    print(output_filename)
 
-    # write headers in
-    print("<Student ID: " + crow_id + ">", file = output_file)
-    print("<Country: " + country + ">", file = output_file)
-    print("<L1: " + L1 + ">", file = output_file)
-    print("<Heritage Spanish Speaker: " + heritage_spanish.capitalize() + ">", file = output_file)
-    print("<Institution: " + institution + ">", file = output_file)
-    print("<Course: ENGL " + course + ">", file = output_file)
-    print("<Mode: " + mode + ">", file = output_file)
-    print("<Length: " + length + ">", file = output_file)
-    print("<Assignment: " + assignment + ">", file = output_file)
-    print("<Draft: " + draft + ">", file = output_file)
-    print("<Year in School: " + year_in_school_numeric + ">", file = output_file)
-    print("<Gender: " + gender + ">", file = output_file)
-    print("<Course Year: " + year + ">", file = output_file)
-    print("<Course Semester: " + semester + ">" , file = output_file)
-    print("<College: " + college + ">", file = output_file)
-    print("<Program: " + program + ">", file = output_file)
-    print("<Proficiency Exam: " + proficiency_exam +">", file = output_file)
-    print("<Exam total: " + exam_total + ">", file = output_file)
-    print("<Exam reading: " + exam_reading + ">", file = output_file)
-    print("<Exam listening: " + exam_listening + ">", file = output_file)
-    print("<Exam speaking: " + exam_speaking + ">", file = output_file)
-    print("<Exam writing: " + exam_writing + ">", file = output_file)
-    print("<Instructor: " + instructor + ">", file = output_file)
-    print("<Section: " + section + ">", file = output_file)
+    # Write headers, line by line.
+    print("<Text>", file=output_file)
+    add_heading('Student IDs', crow_id, output_file)
+    add_heading('Group ID', 'NA', output_file)
+    add_heading('Institution', institution, output_file)
+    add_heading('Course', 'ENGL ' + course , output_file)
+    add_heading('Mode', mode, output_file)
+    add_heading('Length', length, output_file)
+    add_heading('Assignment', assignment, output_file)
+    add_heading('Draft', draft, output_file)
+    add_heading('Course Year', year, output_file)
+    add_heading('Course Semester', semester, output_file)
+    add_heading('Instructor', instructor, output_file)
+    add_heading('Section', section, output_file)
+    print("</Text>", file=output_file)
+    print("", file=output_file)
+
+    print("<Student 1>", file=output_file)
+    add_heading('Student ID', crow_id, output_file)
+    add_heading('Country', country, output_file)
+    add_heading('L1', L1, output_file)
+    add_heading('Heritage Spanish Speaker', heritage_spanish.capitalize(), output_file)
+    add_heading('Year in School', year_in_school_numeric, output_file)
+    add_heading('Gender', gender, output_file)
+    add_heading('College', college, output_file)
+    add_heading('Program', program, output_file)
+    add_heading('Proficiency Exam', proficiency_exam, output_file)
+    add_heading('Exam total', exam_total, output_file)
+    add_heading('Exam reading', exam_reading, output_file)
+    add_heading('Exam listening', exam_listening, output_file)
+    add_heading('Exam speaking', exam_speaking, output_file)
+    add_heading('Exam writing', exam_writing, output_file)
+    print("</Student 1>", file=output_file)
+
     print("<End Header>", file = output_file)
     print("", file = output_file)
     for line in textfile:
@@ -280,6 +291,7 @@ def add_headers_recursive(directory, master):
     files_with_metadata = 0
     files_without_metadata = 0
     results = []
+    print('Running...')
     for dirpath, dirnames, files in os.walk(directory):
         for name in files:
             # Skip non text files.
@@ -295,7 +307,7 @@ def add_headers_recursive(directory, master):
                 files_with_metadata = files_with_metadata + 1
             else:
                 files_without_metadata = files_without_metadata + 1
-
+    print("")
     print('***************************************')
     print('Files found: ' + str(total_files))
     print('Files processed: ' + str(files_with_metadata))
