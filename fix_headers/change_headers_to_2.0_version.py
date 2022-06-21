aea5fbfa7e577f2cae5c583c87d449f9bad2f536
Mark Fullmer <mfullmer@gmail.com>
2022-06-21 10:24:34 -0700

    Capture updates to fix_headers from script in Box

--- fix_headers/change_headers_to_2.0_version.py
+++ fix_headers/change_headers_to_2.0_version.py
@@ -40,10 +40,11 @@ def fix_headers(filename, overwrite=False):
         list_of_headers = headers.split("\n")
 
         output_file = open(output_filename, 'w')
-
+        student_ID_line = list_of_headers[0]
 
         print("<Text>", file=output_file)
-        print(list_of_headers[0], file=output_file)
+        print("<Student IDs: " + student_ID_line[13:], file=output_file)
+        print("<Group ID: NA>", file=output_file)
         print(list_of_headers[2], file=output_file)
         print(list_of_headers[3], file=output_file)
         print(list_of_headers[4], file=output_file)
@@ -55,13 +56,15 @@ def fix_headers(filename, overwrite=False):
         print(list_of_headers[20], file=output_file)
         if "Section" in list_of_headers[21]:
            print(list_of_headers[21], file=output_file)
-        elif:
-            print("Section: NA", file=output_file)
+        else:
+            print("<Section: NA>", file=output_file)
         print("</Text>", file=output_file)
         print("", file=output_file)
-        print("<Student>", file=output_file)
+        print("<Student 1>", file=output_file)
         print(list_of_headers[0], file=output_file)
         print(list_of_headers[1], file=output_file)
+        print("<L1: NA>", file=output_file)
+        print("<Heritage Spanish Speaker: NA>", file=output_file)
         print(list_of_headers[8], file=output_file)
         print(list_of_headers[9], file=output_file)
         print(list_of_headers[12], file=output_file)
@@ -72,11 +75,10 @@ def fix_headers(filename, overwrite=False):
         print(list_of_headers[17], file=output_file)
         print(list_of_headers[18], file=output_file)
         print(list_of_headers[19], file=output_file)
-        print("</Student>", file=output_file)
-        print("", file=output_file)
+        print("</Student 1>", file=output_file)
         if "End Header" in list_of_headers[22]:
-             print(list_of_headers[22], file=output_file)
-        elif: 
+            print(list_of_headers[22], file=output_file)
+        else:
             print("<End Header>", file=output_file)
         print("", file=output_file)
         print(text_body, file=output_file)
@@ -84,10 +86,12 @@ def fix_headers(filename, overwrite=False):
         original_file.close()
         output_file.close()
 
+
 def fix_headers_all_files(directory, overwrite=False):
     for dirpath, dirnames, files in os.walk(directory):
         for name in files:
             fix_headers(os.path.join(dirpath, name), overwrite)
+
 
 if args.dir:
     fix_headers_all_files(args.dir)
