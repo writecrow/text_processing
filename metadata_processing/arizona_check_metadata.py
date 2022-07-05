e0a41994175a0035803e0a53b1ae61e6bf48158a
Mark Fullmer <mfullmer@gmail.com>
2022-07-05 12:32:32 -0700

    Add list of non-matching names to arizona_check_metadaa

--- metadata_processing/arizona_check_metadata.py
+++ metadata_processing/arizona_check_metadata.py
@@ -75,11 +75,33 @@ def get_metadata_for_file(filepath, master):
         # Success! We found a single metadata row corresponding to this file.
         return True
 
+def get_student_name(name):
+    parts = os.path.splitext(name)
+    # Try to split name on hyphen
+    student_filename = parts[0].split('- ')
+    if len(student_filename) > 1:
+        student_filename = student_filename[1]
+    else:
+        student_filename = student_filename[0]
+    student_filename = re.sub(r'\s+', r' ', student_filename)
+    return student_filename.strip()
+
+def get_metadata_names(master):
+    names = []
+    data = master.to_dict(orient="records")
+    for row in data:
+        names.append(row['First Name'] + ' ' + row['Last Name'])
+    return names
+
 
 def check_metadata_recursive(directory, master):
     total_files = 0
     files_with_metadata = 0
     files_without_metadata = 0
+    student_metadata_not_found = []
+    student_filename_not_found = []
+    students_names_from_files = []
+    student_names_from_metadata = get_metadata_names(master)
     for dirpath, dirnames, files in os.walk(directory):
         for name in files:
             # Skip non text files.
@@ -93,6 +115,26 @@ def check_metadata_recursive(directory, master):
                 files_without_metadata = files_without_metadata + 1
             else: 
                 files_with_metadata = files_with_metadata + 1
+            student_name_from_file = get_student_name(name)
+            students_names_from_files.append(student_name_from_file)
+
+    for name in student_names_from_metadata:
+        if name not in students_names_from_files:
+            if name not in student_metadata_not_found:
+                student_metadata_not_found.append(name)
+    print("***************")
+    print("These student names are in the spreadsheet but NOT in the filenames: ")
+    print("***************")
+    print('\n'.join(map(str, student_metadata_not_found)))
+    for student_filename in students_names_from_files:
+        if student_filename not in student_names_from_metadata:
+            if student_filename not in student_filename_not_found:
+                student_filename_not_found.append(student_filename)
+    print("***************")
+    print("These student names are NOT in the spreadsheet but are in the filenames:")
+    print("***************")
+    print('\n'.join(map(str, student_filename_not_found)))
+
     print('***************************************')
     print('Total files found: ' + str(total_files))
     print('Files with matching metadata: ' + str(files_with_metadata))
